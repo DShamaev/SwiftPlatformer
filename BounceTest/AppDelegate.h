1b0aafa93754cb2548bd6a366153949a15e2aca9
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2014-08-14 17:54:16 +0300

    initial commit

---
+++ BounceTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BounceTest
+//
+//  Created by DmitriyShamaev on 11.08.14.
+//  Copyright (c) 2014 DmitriyShamaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BounceTest/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  BounceTest
+//
+
+//  Copyright (c) 2014 DmitriyShamaev. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
